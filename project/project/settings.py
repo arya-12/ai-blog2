24644b64a6625f955b96368343ec9e9366c257c6
Arya samal <aryasamal@Aryas-MacBook-Air.local>
2025-07-23 15:12:31 +0530

    Added settings, staticfiles, and render.yaml for Render deployment

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0g-jxsypa%-7yt7t#@!*g9!ofx6#i9w(@1)88k_jiia8rz#vxj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 
 
